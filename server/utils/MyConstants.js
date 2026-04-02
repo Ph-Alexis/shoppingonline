dd7fc74a56b79fcaf7ff1c1482171996dca7a4b4
Ph-Alexis <phamvo10122003@gmail.com>
2026-04-02 14:13:00 +0700

    first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+  DB_SERVER: "cluster0.ktgnopl.mongodb.net",
+  DB_USER: "xuandb_user",
+  DB_PASS: "Qwerty@123456",
+  DB_DATABASE: "shoppingonline",
+  EMAIL_USER: "xuanphamvo1012@hotmail.com", // Microsoft mail service
+  EMAIL_PASS: "flosweuajlecrqpd",
+  JWT_SECRET: "shoppingonline_secret_key",
+  JWT_EXPIRES: "3600000", // in milliseconds
+};
+
+module.exports = MyConstants;
